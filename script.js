3833e51b227de826697f99c2226ec992866ded1f
“doganirmak” <dogancan.irmak@gmail.com>
2023-01-01 22:35:28 +0300

    d2

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("test");
+let sonuc;
+let a = 10, b = 20, c = 30;
+
+sonuc = a + b;
+sonuc = a - b;
+sonuc = a * b;
+sonuc = a / b;
+sonuc = c % a;
+sonuc = a++;
+sonuc = a--;
+sonuc = a *= b;
+
+
+console.log(sonuc);
+console.log(a);
