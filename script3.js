3df4a1c9f66ed013abed9dd59089ce5d45121227
“doganirmak” <dogancan.irmak@gmail.com>
2023-01-03 21:26:31 +0300

    d2

---
+++ script3.js
@@ -0,0 +1,18 @@
+let ad = "Doğan";
+let soyad = "Irmak";
+let yas = 29;
+let sehir = "Londra";
+
+let mesaj =
+  "Benim adım " +
+  ad +
+  " ve soyadım " +
+  soyad +
+  ". " +
+  sehir +
+  "'da yaşıyorum." +
+  " Emekliliğe " +
+  (65 - yas) +
+  " yılım kaldı.";
+
+console.log(mesaj);
