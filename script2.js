7062f5e1804cd64321c3fd400669f259b0ac7a95
“doganirmak” <dogancan.irmak@gmail.com>
2023-01-03 21:26:16 +0300

    d1

--- script2.js
+++ script2.js
@@ -6,9 +6,9 @@ if (10 < x < 50) {
 
 /*********/
 
-let x1 = 13;
+let x1 = -13;
 
-if (x1 % 2 == 0 && x1 > 0) {
+if (Math.abs(x1) % 2 == 0 && x1 > 0) {
   console.log(x1 + ": pozitif çift bir sayıdır");
 } else {
   console.log(x1 + ": pozitif tek bir sayıdır");
@@ -26,10 +26,10 @@ if (a < b && a < c) {
 
 /*********/
 
-let vize1 = 01;
-let vize2 = 10;
-let final = 61;
-let ort = (vize1 + vize2) * 0.4 + final * 0.6;
+let vize1 = 50;
+let vize2 = 50;
+let final = 20;
+let ort = (((vize1 + vize2) / 2) * 0.4) + final * 0.6;
 
 if (ort > 50) {
   if (final > 50) {
