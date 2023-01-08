610a5b7762034d1869c4fb32b5989b9d4dbdd01c
“doganirmak” <dogancan.irmak@gmail.com>
2023-01-08 17:13:13 +0300

    d5

---
+++ script5.js
@@ -0,0 +1,19 @@
+let url = "https://www.gigapine.com/";
+let firmaAdi = "Gigapine Teknoloji Bilgi Sistemleri";
+
+// 1
+console.log(url.length);
+
+//2
+countWord = firmaAdi.split(" ").length;
+console.log(countWord);
+
+//3
+urlInclude = url.startsWith("https");
+console.log(urlInclude);
+
+//4
+firmaAdiInclude = firmaAdi.includes("Eğitimi");
+console.log(firmaAdiInclude);
+
+//5
